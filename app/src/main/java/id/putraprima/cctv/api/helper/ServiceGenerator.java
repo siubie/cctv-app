a0323c847a5a39ec405f8dd18a822dfb639e7db0
Putra Prima Arhandi <putraprima@gmail.com>
2020-03-01 20:46:35 +0700

    push envelope

--- app/src/main/java/id/putraprima/cctv/api/helper/ServiceGenerator.java
+++ app/src/main/java/id/putraprima/cctv/api/helper/ServiceGenerator.java
@@ -1,5 +1,6 @@
 package id.putraprima.cctv.api.helper;
 
+import id.putraprima.cctv.api.converters.UnwrapEnvelopeConverterFactory;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
@@ -12,6 +13,7 @@ public class ServiceGenerator {
     private static Retrofit.Builder builder =
             new Retrofit.Builder()
                     .baseUrl(BASE_URL)
+                    .addConverterFactory(new UnwrapEnvelopeConverterFactory())
                     .addConverterFactory(GsonConverterFactory.create());
 
     private static Retrofit retrofit = builder.build();
